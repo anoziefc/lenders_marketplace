cd01ea6ee497e6066b1e2f095ac5f4f2d8faad20
Anadudev <anadudev@gmail.com>
2025-07-31 15:03:13 +0100

    feat(form): add currency prefix on focus in funding amount input
    Add automatic £ prefix when user focuses on funding amount input field if not already present. Also created new journey and results pages with consistent layout and contact information.

---
+++ frontend/src/app/results/page.tsx
@@ -0,0 +1,42 @@
+import Form from "@/components/Form";
+import Rating from "@/components/Ratings";
+import TopBar from "@/components/TopBar";
+import Image from "next/image";
+import Link from "next/link";
+
+const Page = () => {
+  return (
+    <div
+      className={
+        "p-4 flex flex-col gap-4 items-center justify-center min-h-screen"
+      }
+    >
+      <TopBar />
+      <div className="flex items-center content-center w-full justify-center">
+        <Form />
+      </div>
+      <div className="">
+        <p>
+          Need some help? Give us a call on{" "}
+          <Link className="blue" href="tell:">
+            02081232841
+          </Link>{" "}
+          or email us at
+          <Link className="blue" href="mailto:hello@fundonion.com">
+            {" "}
+            hello@fundonion.com
+          </Link>
+        </p>
+        <p>
+          Copyright © FundOnion Limited, 2024 | Registered in England & Wales
+          (co. number 12743839)
+        </p>
+      </div>
+      <div className="">
+        <Rating />
+      </div>
+    </div>
+  );
+};
+
+export default Page;
